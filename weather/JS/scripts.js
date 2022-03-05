20c6dc24787ada196a39b117dcc6612648672fd0
unknown <delly31@hotmail.com>
2022-03-05 22:39:11 +0100

    adding weather

---
+++ weather/JS/scripts.js
@@ -0,0 +1,34 @@
+let d = new Date()
+let daysOfTheWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    
+]
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+
+]
+let dayname = daysOfTheWeek[d.getDay()]
+let monthname = months[d.getMonth()]
+document.querySelector("#year").innerHTML = new Date().getFullYear()
+document.querySelector("#dateofnow").innerHTML = `${dayname}, ${d.getDate()} ${monthname} ${d.getFullYear()}`
+
+function toggleMenu() {
+	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
