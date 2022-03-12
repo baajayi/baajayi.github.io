237da159266fac689bd9c9a8bf4373b981079cc9
unknown <delly31@hotmail.com>
2022-03-12 19:08:25 +0100

    updating weather

--- weather/JS/scripts.js
+++ weather/JS/scripts.js
@@ -41,11 +41,3 @@ if (daysOfTheWeek[d.getDay()]==="Friday") {
 	document.body.insertBefore(banner,document.body.childNodes[0]).style.fontFamily = "Josefin Sans"
     document.body.insertBefore(banner,document.body.childNodes[0]).style.fontWeight = "bold"   
 }
-const temp = parseFloat(document.getElementById("temp").innerHTML, 10)
-const speed = parseFloat(document.getElementById("speed").innerHTML, 10)
-if (temp <= 50 && speed > 3){
-    const chill = 35.74 + (0.6215 * temp) - (35.75 * (speed^0.16)) + (0.4275 * temp * (speed^0.16))
-document.querySelector("#chill").innerHTML = chill.toFixed(2)
-} else {
-    document.querySelector("#chill").innerHTML = "N/A"
-}
