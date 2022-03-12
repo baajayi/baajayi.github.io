818dded3365689ef7ba5de76e14282d5c131db3f
unknown <delly31@hotmail.com>
2022-03-12 06:32:09 +0100

    updating weather

--- weather/JS/scripts.js
+++ weather/JS/scripts.js
@@ -33,7 +33,14 @@ function toggleMenu() {
 	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
 if (daysOfTheWeek[d.getDay()]==="Friday") {
-    let banner = document.createElement("h2")
-    banner.textContent = "Preston Pancakes in the Park! 9:00 a.m. Saturday at the city park pavilion."
+    let banner = document.createElement("div")
+    banner.className = "display"
+    banner.innerHTML = "Preston Pancakes in the Park! 9:00 a.m. Saturday at the city park pavilion."
+    document.body.insertBefore(banner,document.body.childNodes[0]).style.display = "block";
+    document.body.insertBefore(banner,document.body.childNodes[0]).style.textAlign = "center";
+	document.body.insertBefore(banner,document.body.childNodes[0]).style.fontFamily = "Josefin Sans"
+    document.body.insertBefore(banner,document.body.childNodes[0]).style.fontWeight = "bold";
+
+	
     
 }
