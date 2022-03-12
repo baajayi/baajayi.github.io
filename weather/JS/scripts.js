766cfef70d31111130a48286b71ae2410c11bfbe
unknown <delly31@hotmail.com>
2022-03-12 18:28:15 +0100

    updating weather

--- weather/JS/scripts.js
+++ weather/JS/scripts.js
@@ -36,11 +36,16 @@ if (daysOfTheWeek[d.getDay()]==="Friday") {
     let banner = document.createElement("div")
     banner.className = "display"
     banner.innerHTML = "Preston Pancakes in the Park! 9:00 a.m. Saturday at the city park pavilion."
-    document.body.insertBefore(banner,document.body.childNodes[0]).style.display = "block";
-    document.body.insertBefore(banner,document.body.childNodes[0]).style.textAlign = "center";
+    document.body.insertBefore(banner,document.body.childNodes[0]).style.display = "block"
+    document.body.insertBefore(banner,document.body.childNodes[0]).style.textAlign = "center"
 	document.body.insertBefore(banner,document.body.childNodes[0]).style.fontFamily = "Josefin Sans"
-    document.body.insertBefore(banner,document.body.childNodes[0]).style.fontWeight = "bold";
-
-	
-    
+    document.body.insertBefore(banner,document.body.childNodes[0]).style.fontWeight = "bold"   
+}
+const temp = parseFloat(document.getElementById("temp").innerHTML, 10)
+const speed = parseFloat(document.getElementById("speed").innerHTML, 10)
+if (temp <= 50 && speed > 3){
+    const chill = 35.74 + (0.6215 * temp) - (35.75 * (speed^0.16)) + (0.4275 * temp * (speed^0.16))
+document.querySelector("#chill").innerHTML = chill.toFixed(2)
+} else {
+    document.querySelector("#chill").innerHTML = "N/A"
 }
