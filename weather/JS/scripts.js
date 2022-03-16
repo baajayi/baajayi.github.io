cd87b388b31fa0beb4e4a94f51566f4d424e862f
unknown <delly31@hotmail.com>
2022-03-16 22:40:29 +0100

    updating weather

--- weather/JS/scripts.js
+++ weather/JS/scripts.js
@@ -42,3 +42,33 @@ if (daysOfTheWeek[d.getDay()]==="Friday") {
 	document.body.insertBefore(banner,document.body.childNodes[0]).style.fontFamily = "Josefin Sans"
     document.body.insertBefore(banner,document.body.childNodes[0]).style.fontWeight = "bold"   
 }
+const images = document.querySelectorAll("[data-src]")
+
+
+function preloadImage(img) {
+    const src = img.getAttribute("data-src")
+    if (!src) {
+        return;
+    } 
+    img.src = src
+}
+
+const imgOptions = {
+    threshold: 1, 
+    rootMargin: "0px 0px 200px 0px"
+}
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            preloadImage(entry.target);
+            imgObserver.unobserve(entry.target)
+        }
+    })
+}, imgOptions)
+
+images.forEach(image => {
+    imgObserver.observe(image)
+})
+
