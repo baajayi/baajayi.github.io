1df913a7b2a11053a043c3f1846541b7a18bdf8e
unknown <delly31@hotmail.com>
2022-03-19 21:08:03 +0100

    updating weather

--- weather/JS/scripts.js
+++ weather/JS/scripts.js
@@ -74,4 +74,10 @@ images.forEach(image => {
 function adjustRating(rating) {
     document.getElementById("ratingvalue").innerHTML = rating;
 }
-
+function selectResponse() {
+	const s = document.querySelector("#selected")
+	const sel = document.querySelector("#storm-region");
+	s.style.display = "block";
+	s.textContent = sel.value;
+	
+}
